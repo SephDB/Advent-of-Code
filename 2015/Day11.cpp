c285df3e3d6638bb37de7c9f17dbb0e4f714acc5
Seph De Busser <seph.debusser@gmail.com>
2019-12-29 12:28:16 +0100

    2015/Day 11

---
+++ 2015/Day11.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <algorithm>
+
+bool two_pairs(std::string_view p) {
+    auto a = std::adjacent_find(p.begin(),p.end());
+    return a < p.end()-3 and std::adjacent_find(a+2,p.end()) != p.end();
+}
+
+bool incrementing_triple(std::string_view p) {
+    for(int i = 2; i < p.size(); ++i) {
+        if(p[i] == p[i-1]+1 and p[i-1] == p[i-2]+1) return true;
+    }
+    return false;
+}
+
+bool valid(std::string_view p) {
+    return p.find_first_of("iol") == p.npos and two_pairs(p) and incrementing_triple(p);
+}
+
+void inc(std::string& s) {
+    auto index = s.size()-1;
+    while(s[index] == 'z') s[index--] = 'a';
+    s[index]++;
+}
+
+int main() {
+    std::string start = "hxbxwxba";
+    
+    while(not valid(start)) inc(start);
+    std::cout << "Part 1: " << start << '\n';
+    inc(start);
+    while(not valid(start)) inc(start);
+    std::cout << "Part 2: " << start << '\n';
+}
