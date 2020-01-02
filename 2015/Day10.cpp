85306bdafb93c4e16b07a7606d23dab019a6f4d7
Seph De Busser <seph.debusser@gmail.com>
2020-01-02 21:46:29 +0100

    2015/Day 10 - Heapless
    Had some fun creating a non-allocating version of this. Takes ages to compile for actual input size.

--- 2015/Day10.cpp
+++ 2015/Day10.cpp
@@ -34,7 +34,89 @@ auto apply(std::string number, int iterations) {
     return next_length(number);
 }
 
+namespace HeapLess {
+    enum class IterState {COUNT,CURRENT,END};
+    template<typename I>
+    struct Iter {
+        I begin,end;
+        IterState state;
+        int count;
+        char c;
+        Iter& operator++() {
+            switch(state) {
+                case IterState::COUNT:
+                    state = IterState::CURRENT;
+                    break;
+                case IterState::CURRENT:
+                    if(begin == end) {
+                        state = IterState::END;
+                        break;
+                    }
+                    c = *begin;
+                    count = 0;
+                    while(begin != end and *begin == c) {++count; ++begin;}
+                    if(count == 0) {
+                        state = IterState::END;
+                    } else {
+                        state = IterState::COUNT;
+                    }
+                    break;
+            }
+            return *this;
+        }
+        char operator*() const {
+            switch(state) {
+                case IterState::COUNT:
+                    return '0'+count;
+                case IterState::CURRENT:
+                    return c;
+            }
+            throw;
+        }
+        bool operator==(Iter o) const {return begin == o.begin and state == o.state;}
+        bool operator!=(Iter o) const {return !(*this == o);}
+    };
+    template<typename I>
+    Iter(I,I,IterState) -> Iter<I>;
+    
+    template<int N>
+    struct Seq;
+
+    template<>
+    struct Seq<0> {
+        Seq(std::string_view a) : s(a) {};
+        std::string_view s;
+        auto begin() const {
+            return s.begin();
+        }
+        auto end() const {
+            return s.end();
+        }
+    };
+
+    template<int N>
+    struct Seq {
+        Seq(std::string_view a) : next(a) {};
+        auto begin() const {
+            return ++Iter{next.begin(),next.end(),IterState::CURRENT};
+        }
+        auto end() const {
+            return Iter{next.end(),next.end(),IterState::END};
+        }
+
+        Seq<N-1> next;
+    };
+
+    template<int N>
+    int size(std::string_view a) {
+        int count = 0;
+        for(auto c : Seq<N>(a)) ++count;
+        return count;
+    }
+}
+
 int main() {
+    std::cout << "Heapless: " << HeapLess::size<15>("1113122113") << '\n'; //About the biggest that compiles in a reasonable time
     std::cout << "Part 1: " << apply("1113122113",40) << '\n';
     std::cout << "Part 2: " << apply("1113122113",50) << '\n';
 }
