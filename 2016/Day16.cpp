e0a1e4b5431412df31d82ce087ce42c6946a9b8e
Seph De Busser <seph.debusser@gmail.com>
2020-01-13 16:08:59 +0100

    2016/Day 16
    Limited stack space strikes once again!

--- 2016/Day16.cpp
+++ 2016/Day16.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 #include <bitset>
+#include <memory>
 #include <algorithm>
 
-using Curve = std::bitset<272>;
-
+template<typename Curve>
 int next(Curve& a, int current) {
     auto next_size = current*2+1;
     if(next_size > a.size()) next_size = a.size();
@@ -15,44 +15,51 @@ int next(Curve& a, int current) {
     return next_size;
 }
 
-void output(Curve c, int size) {
-    auto s = c.to_string();
-    std::reverse(s.begin(),s.end());
-    s = s.substr(0,size);
-    std::cout << s << '\n';
-}
-
-void output_checksum(Curve c, int stride) {
+template<typename Curve>
+void output_checksum(Curve& c, int stride) {
     for(int i = 0; i < c.size(); i += stride) {
         std::cout << c[i];
     }
     std::cout << '\n';
 }
-/*
-   110010110100110010110100
+/* 110010110100110010110100
 1: .1.1.0.1.0.1.1.1.0.1.0.1 ~c^(c>>1), stride 2
 2: ...1...0...0...1...0...0 ~c^(c>>2), stride 4
 3: .......0.......0.......1 ~c^(c>>4), stride 8
 */
+template<typename Curve>
 int checksum(Curve& c) {
     int stride = 1;
+    //Copy bc it's too expensive to keep on the stack
+    auto c_copy = std::make_unique<Curve>(c);
+    auto& c2 = *c_copy;
     do {
-        c = ~(c ^ (c >> stride));
+        c2 >>= stride;
+        c ^= c2;
+        c.flip();
+        c2 = c;
         stride *= 2;
     } while(c.size() / stride % 2 == 0);
     return stride;
 }
 
-int main() {
-    std::string input = "10001001100000001";
-    std::reverse(input.begin(),input.end());
-    Curve start(input.data());
+template<int N>
+void solve(std::string_view input) {
+    auto sp = std::make_unique<std::bitset<N>>(input.data());
+    auto& start = *sp;
     auto s = input.size();
     while(s < start.size()) {
         s = next(start,s);
     }
-    std::cout << '\n';
     auto stride = checksum(start);
-    std::cout << "Part 1: ";
     output_checksum(start,stride);
+}
+
+int main() {
+    std::string input = "10001001100000001";
+    std::reverse(input.begin(),input.end());
+    std::cout << "Part 1: ";
+    solve<272>(input);
+    std::cout << "Part 2: ";
+    solve<35651584>(input);
 }
