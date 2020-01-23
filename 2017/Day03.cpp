785f2d4ce826723a1a445536c79f477c37d550bf
Seph De Busser <seph.debusser@gmail.com>
2020-01-23 13:10:05 +0100

    2017/Day 3

--- 2017/Day03.cpp
+++ 2017/Day03.cpp
@@ -1,6 +1,8 @@
 #include <array>
 #include <iostream>
 #include <cmath>
+#include <numeric>
+#include <deque>
 
 auto distance(int i) {
     //Numbers on the diagonals are of the form n*(n-1)+1
@@ -20,6 +22,41 @@ auto distance(int i) {
     return std::abs(x) + std::abs(y);
 }
 
+auto sums(int input) {
+    //Example starting at position 26, which locations to sum together
+    /*
+    26=10,25
+    27=10,11,25,26
+    28=10,11,12,27
+    29=11,12,13,28
+    30=12,13,29
+    31=13,30
+    32=13,14,30,31
+    33=13,14,15,32
+    34=14,15,16,33
+    35=15,16,17,34
+    36=16,17,35
+    37=17,36
+    38=17,18,36,37
+    ...
+    */
+    std::deque<int> nums{1,1,2,4,5,10,11};
+    for(int iteration = 6; ; ++iteration) {
+        nums.push_back(nums.back()*2+nums[1]);
+        if(nums.back() >= input) return nums.back();
+
+        int len = iteration/2 - 1;
+        for(int len = iteration/2 - 1; len > 0; --len) {
+            auto next = std::accumulate(nums.begin(),nums.begin()+std::min(3,len),nums.back(),std::plus{});
+            if(next >= input) return next;
+            nums.push_back(next);
+            if(len > 1) nums.pop_front();
+        }
+    }
+}
+
 int main() {
-    std::cout << "Part 1: " << distance(277678) << '\n';
+    constexpr auto input = 277678;
+    std::cout << "Part 1: " << distance(input) << '\n';
+    std::cout << "Part 2: " << sums(input) << '\n';
 }
