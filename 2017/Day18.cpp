35debe5debe86e6b6ccafe2cf5553dfc2cee4002
Seph De Busser <seph.debusser@gmail.com>
2020-12-01 13:47:20 +0100

    2017/Day 18 - finished part 2

--- 2017/Day18.cpp
+++ 2017/Day18.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string_view>
 #include <array>
-#include <map>
+#include <deque>
 #include <charconv>
 #include <numeric>
 #include <algorithm>
@@ -35,79 +35,113 @@ enum class Type {
 struct Ins {
     Type type;
     int reg;
-    int64_t second = 0;
-    bool is_constant = true;
+    int64_t arg = 0;
+    bool reg_constant = false;
+    bool arg_constant = true;
 };
 
 auto parse(std::string_view input) {
     std::vector<Ins> instructions;
     split(input,'\n',[&](std::string_view line) {
+        Ins i{};
         auto ins = line.substr(0,3);
-        int reg = line[4] - 'a';
+        int reg = line[4];
+        if(reg >= 'a' && reg <= 'z') {
+            i.reg = reg - 'a';
+            i.reg_constant = false;
+        } else {
+            i.reg = reg - '0';
+            i.reg_constant = true;
+        }
         if(ins == "snd") {
-            instructions.push_back({Type::snd,reg});
+            i.type = Type::snd;
         } else if(ins == "rcv") {
-            instructions.push_back({Type::rcv,reg});
+            i.type = Type::rcv;
         } else {
             Type t = Type::set;
             if(ins == "add") t = Type::add;
             else if(ins == "mul") t = Type::mul;
             else if(ins == "mod") t = Type::mod;
             else if(ins == "jgz") t = Type::jgz;
+            
+            i.type = t;
 
-            auto second = line.substr(6);
-            if(second[0] >= 'a' && second[0] <= 'z') {
-                instructions.push_back({t,reg,second[0]-'a',false});
+            auto arg = line.substr(6);
+            if(arg[0] >= 'a' && arg[0] <= 'z') {
+                i.arg = arg[0]-'a';
+                i.arg_constant = false;
             } else {
-                instructions.push_back({t,reg,to_int(second),true});
+                i.arg = to_int(arg);
+                i.arg_constant = true;
             }
         }
+        instructions.push_back(i);
     });
     return instructions;
 }
 
+struct Prog {
+    std::array<int64_t,26> regs = {0};
+    int pc = 0;
+    std::deque<int64_t> input = {};
+    int total_sent = 0;
+
+    int64_t get(Ins s) const {
+        if(s.arg_constant) return s.arg;
+        return regs[s.arg];
+    }
+    bool run(const std::vector<Ins>& instructions, Prog& other) {
+        while(pc >= 0 && pc < instructions.size()) {
+            auto current = instructions[pc];
+            switch(current.type) {
+                case Type::snd:
+                    total_sent++;
+                    other.input.push_back(current.reg_constant ? current.reg : regs[current.reg]);
+                    break;
+                case Type::set:
+                    regs[current.reg] = get(current);
+                    break;
+                case Type::add:
+                    regs[current.reg] += get(current);
+                    break;
+                case Type::mul:
+                    regs[current.reg] *= get(current);
+                    break;
+                case Type::mod:
+                    regs[current.reg] %= get(current);
+                    break;
+                case Type::rcv:
+                    if(input.empty()) return false;
+                    regs[current.reg] = input.front();
+                    input.pop_front();
+                    break;
+                case Type::jgz:
+                    if((current.reg_constant ? current.reg : regs[current.reg]) > 0) {
+                        pc += get(current) - 1;
+                    }
+                    break;
+            }
+            ++pc;
+        }
+        return true;
+    }
+};
+
 void solution(std::string_view input) {
     auto instructions = parse(input);
-    std::array<int64_t,26> regs;
-    std::fill(regs.begin(),regs.end(),0);
-    int sound_played = 0;
-    int ins = 0;
-    auto get = [&regs](Ins s) {
-        if(s.is_constant) return s.second;
-        return regs[s.second];
-    };
-    while(ins >= 0 && ins < instructions.size()) {
-        auto current = instructions[ins];
-        switch(current.type) {
-            case Type::snd:
-                sound_played = regs[current.reg];
-                break;
-            case Type::set:
-                regs[current.reg] = get(current);
-                break;
-            case Type::add:
-                regs[current.reg] += get(current);
-                break;
-            case Type::mul:
-                regs[current.reg] *= get(current);
-                break;
-            case Type::mod:
-                regs[current.reg] %= get(current);
-                break;
-            case Type::rcv:
-                if(regs[current.reg] != 0) {
-                    std::cout << "Part 1: " << sound_played << '\n';
-                    return;
-                }
-                break;
-            case Type::jgz:
-                if(regs[current.reg] > 0) {
-                    ins += get(current) - 1;
-                }
-                break;
-        }
-        ++ins;
+    Prog p0,p1;
+    p0.regs['p'-'a'] = 0;
+    p1.regs['p'-'a'] = 1;
+    bool terminated0 = false;
+    bool terminated1 = false;
+    p0.run(instructions,p1);
+    std::cout << "Part 1: " << p1.input.back() << '\n';
+    while(p0.input.size() || p1.input.size()) {
+        terminated1 = p1.run(instructions,p0);
+        terminated0 = p0.run(instructions,p1);
+        if(terminated0 && terminated1) break;
     }
+    std::cout << "Part 2: " << p1.total_sent << '\n';
 }
 
 std::string_view input = R"(set i 31
